2ff4b473459c7bdf9b767d7b54d54081670b009f
Yasuhei Nakamura <67479901+yasuhei@users.noreply.github.com>
2023-04-12 20:30:11 -0300

    concluido

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Basket from '../views/Basket.vue'
+import Product from '../views/Product.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/basket',
     name: 'Basket',
     component: Basket
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product
   },
   
 ]
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,52 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    
+    products: [],
+    productInBag: []
   },
   mutations: {
-    
+    loadProducts(state,products) {
+      state.products = products
+    },
+    loadBag(state,products) {
+      state.productInBag = products
+    },
+    addToBag(state, product) {
+      state.productInBag.push(product)
+      localStorage.setItem('productInBag',JSON.stringify(state.productInBag))
+    },
+    removeFromBag(state, productId) {
+      var updateBag = state.productInBag.filter(item => productId != item.id);
+      state.productInBag = updateBag
+      localStorage.setItem('productInBag',JSON.stringify(state.productInBag))
+
+    }
+
   },
   actions: {
+    loadProducts({commit}) {
+      axios.get('https://fakestoreapi.com/products').then(response => {
+        commit('loadProducts',response.data)
+      })
+    },
+    loadBag({commit}) {
+      if(localStorage.getItem('productInBag')){
+        commit('loadBag', JSON.parse(localStorage.getItem('productInBag')))
 
+      }
+    },
+    addToBag({commit}, product) {
+      commit('addToBag', product)
+    },
+
+
+    removeFromBag({commit}, productId) {
+      if(confirm('Você deseja remover esse item do carrinho?')){
+        commit('removeFromBag', productId)
+      }
+    }
   },
   modules: {
   }
